4298bc0a61edacbf109d29818cfde091d068cd49
veetee207 <v.terehov207@gmail.com>
2020-08-20 00:28:09 -0400

    commit #2 -- empty field and no longer 50 letters

--- file12.js
+++ file12.js
@@ -29,11 +29,48 @@ const numberOfFilms = +prompt("How many movies have you seen already?"),
         actors: {},
         genres: [],
         privat: false,
-    },
+    };
 
-    lastMovie = prompt('Which movie do you watch last?'),
-    movieRate = +prompt('How do you rate it?');
+// lastMovie = prompt('Which movie do you watch last?'),
+// movieRate = +prompt('How do you rate it?');
 
-personalMovieDB['movies'][lastMovie] = movieRate;
+// personalMovieDB['movies'][lastMovie] = movieRate;
+
+//     1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+// 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+// отменить ответ или ввести название фильма длинее, чем 50 символов.Если это происходит -
+//     возвращаем пользователя к вопросам опять
+
+// 3) При помощи условий проверить personalMovieDB.count, и если он меньше 10 - вывести сообщение "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+//     "Вы киноман".А если не подошло ни к одному варианту - "Произошла ошибка"
+
+// 4) Потренироваться и переписать цикл еще двумя способами * /
+
+for (let i = 0; i < 2; i++) {
+
+    const lastMovie = prompt('Which movie do you watch last?');
+    if (lastMovie == '' || lastMovie.length > 50) {
+        alert("The answer can not be empty or longer 50 symbols!");
+        i--;
+        continue;
+    }
+    const movieRate = +prompt('How do you rate it?');
+    personalMovieDB.movies[lastMovie] = movieRate;
+}
+
+if (personalMovieDB.count <= 10) {
+    console.log('very few films');
+
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('classical tv fan');
+
+} else if (personalMovieDB.count >= 30) {
+    console.log('addicted tv fan');
+
+} else {
+    console.log('Some mistake is occured ! ');
+}
+
 
 console.log(personalMovieDB);
